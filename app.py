3a7e99fe84a3f4b5d0799cd7268b12810a7e7850
Peter Mankowski <petermankowski510@gmail.com>
2024-12-26 21:06:57 -0800

    first commit - commiting all files to the repository

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import streamlit_extras.add_vertical_space import add_vertical_space
+
+#Sidebar contents
+with st.sitebar:
+    st.title('LLM Chat App')
+    st.markdown('''
+    ## About
+    This app is an LLM-powered chatbot built using:
+    - [Streamlit](https://streamlit.io/)
+    - [LangChain](https://python.langchain.com/)streamlit run app.py
+    - [OpenAI](https://platform.openai.com/docs/models) LLM model
+    ''')
+    add_vertical_space(5)
+    st.write('Made with by [Prompt Engineer](http://youtube.com/@engineeringprompt)')
